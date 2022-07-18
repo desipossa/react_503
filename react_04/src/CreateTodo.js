5362b907473f040a498e2f0f689b6b070c6f78b9
jjamin <c1jang@hanmail.net>
2022-07-18 21:50:49 +0900

    20220718

---
+++ react_04/src/CreateTodo.js
@@ -0,0 +1,9 @@
+const CreateTodo = ({ onCleate, onChange }) => {
+    return (
+        <>
+            <input name="content" onChange={onChange} />
+            <button onClick={onCleate}>할일 추가</button>
+        </>
+    )
+}
+export default CreateTodo;
---
+++ react_04/src/TodoList.js
@@ -0,0 +1,10 @@
+const TodoList = ({ TodoList }) => {
+    return (
+        <ul>
+            {console.log(TodoList)}
+            {TodoList.map(it => <li key={it.id}>{it.content}</li>)}
+        </ul>
+    )
+}
+
+export default TodoList;
