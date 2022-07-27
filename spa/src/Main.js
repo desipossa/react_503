3e8eb578297d8a2b66e49fc27a76e10a557bd17d
jjamin <c1jang@hanmail.net>
2022-07-27 21:46:11 +0900

    20220727

--- spa/src/Main.js
+++ spa/src/Main.js
@@ -1,6 +1,15 @@
 const Main = () => {
+    const DATA = [
+        { id: 1, content: '우리나라 대한민국' },
+        { id: 2, content: '오늘은 메로나 먹을 거임' },
+        { id: 3, content: '내일은 자야징...' },
+    ]
     return (
-        <div className='Main'>Main</div>
+        <div className='Main'>
+            {
+                DATA.map(el => <li key={el.id}>{el.content}</li>)
+            }
+        </div>
     )
 }
 export default Main;
--- spa/src/Nav.js
+++ spa/src/Nav.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
 const Nav = () => {
     return (
-        <div className='Nav'>Nav</div>
+        <nav className='Nav'>
+            <ul>
+                <li><Link to='/'>menu01</Link></li>
+                <li><Link to='/Tab'>menu02</Link></li>
+                <li><Link to='/1'>menu03</Link></li>
+                <li><Link to='/2'>menu04</Link></li>
+                <li><Link to='/3'>menu05</Link></li>
+            </ul>
+        </nav>
     )
 }
 export default Nav;
